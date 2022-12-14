fa5750c1138fbafe6041304602ec618833b00e2b
guille garcia <ggarciacardiel@gmail.com>
2022-12-14 18:06:16 +0100

    added partials > card to /riders

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ app.use(express.static('public'));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views'); // app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(__dirname + '/views/partials'); //hbs.registerPartials.path.join(__dirname, '/views/partials')
+hbs.registerHelper('toUpperCase', function(str) { //without this the page was highlighting an error.
+  return str.toUpperCase();
+});
 
 app.get('/', (req, res, next) => {
   res.status(200).render('home');
@@ -21,6 +24,34 @@ app.get('/works', (req, res, next) => {
 
 app.get('/gallery', (req, res, next) => {
   res.status(200).render('gallery')
+})
+
+app.get('/riders', (req, res, next) => {
+  const riders = {
+    riders: [
+      {
+        name: 'Remco Evenepoel',
+        age: 22,
+        country: 'Belgium'
+      },
+      {
+        name: 'Tadej Pogačar',
+        age: 24,
+        country: 'Slovenia'
+      },
+      {
+        name: 'Egan Bernal',
+        age: 25,
+        country: 'Colombia'
+      },
+      {
+        name: 'Enric Mas',
+        age: 27,
+        country: 'Spain'
+      }
+    ]
+  }
+  res.render('riders', riders);
 })
 
 app.listen(3000, () => {console.log('App running')})
