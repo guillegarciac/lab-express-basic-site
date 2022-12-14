57788168dde17807468ad868347a2937f38f99c0
guille garcia <ggarciacardiel@gmail.com>
2022-12-14 17:06:21 +0100

    added hbs + layout + updated app.js

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require ('express');
 const app = express();
+const hbs = require('hbs');
 
 app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views'); // app.set('views', path.join(__dirname, 'views'));
+hbs.registerPartials(__dirname + '/views/partials'); //hbs.registerPartials.path.join(__dirname, '/views/partials')
 
 app.get('/', (req, res, next) => {
-  res.status(200).sendFile(__dirname + '/views/home.html')
+  res.status(200).render('home');
 })
 
 app.get('/about', (req, res, next) => {
-  res.sendFile(__dirname + '/views/about.html')
+  res.status(200).render('about')
 })
 
 app.get('/works', (req, res, next) => {
-  res.sendFile(__dirname + '/views/works.html')
+  res.status(200).render('works')
 })
 
 app.get('/gallery', (req, res, next) => {
-  res.sendFile(__dirname + '/views/gallery.html')
+  res.status(200).render('gallery')
 })
 
 app.listen(3000, () => {console.log('App running')})
